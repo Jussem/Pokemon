9c27f9db54b3404847f43ee90f0925f4cf87e4b0
Juan Jose Morales Aristizabal <juanj.moralesa@autonoma.edu.co>
2025-04-01 17:06:08 -0500

    Implementacion de la interfaz Pokemon Planta

---
+++ src/autonoma/pokemon/models/PokemonPlanta.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package autonoma.pokemon.models;
+
+/**
+ *
+ * @author juanb
+ */
+public interface PokemonPlanta {
+    //metodo abstracto para el ataque "Paralizar"
+    //@return String
+    public abstract String ataqueParalizar();
+    
+    //metodo abstracto para el ataque "Hoja Afilada"
+    //@return String
+    public abstract String ataqueHojaAfilada();
+    
+    //metodo abstracto para el ataque "Drenaje"
+    //@return String
+    public abstract String ataqueDrenaje();
+    
+    //metodo abstracto para el ataque "Latigocepa"
+    //@return String
+    public abstract String ataqueLatigocepa();
+}

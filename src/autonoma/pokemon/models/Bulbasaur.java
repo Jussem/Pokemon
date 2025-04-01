24525090adfdbf839a30b3a69aa43c8f8d3005d3
Juan Jose Morales Aristizabal <juanj.moralesa@autonoma.edu.co>
2025-04-01 17:31:34 -0500

    Implementacion de la clase Bulbasour

---
+++ src/autonoma/pokemon/models/Bulbasaur.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.pokemon.models;
+
+/**
+ *
+ * @author juanb
+ */
+public class Bulbasaur extends Pokemon implements PokemonPlanta {
+    //CONSTRUCTOR
+    public Bulbasaur(int numPokedex,String nombre, float pesoPokemon, String temporada){
+        super(numPokedex, nombre, pesoPokemon, temporada);
+    }
+    //metodo para uasr el ataque "Placaje"
+    @Override
+    public String ataquePlacaje() {
+        throw new UnsupportedOperationException("Bulbasaur ataca con 'Placaje'"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    //metodo para uasr el ataque "Arañazo"
+    @Override
+    public String ataqueAranazo() {
+        throw new UnsupportedOperationException("Bulbasaur ataca con 'Aranazo'"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    //metodo para uasr el ataque "Mordisco"
+    @Override
+    public String ataqueMordisco() {
+        throw new UnsupportedOperationException("Bulbasaur ataca con 'Mordisco'"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    //metodo para uasr el ataque "Paralizar"
+    @Override
+    public String ataqueParalizar() {
+        throw new UnsupportedOperationException("Bulbasaur ataca con 'Paralizar'"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    //metodo para uasr el ataque "Hoja Afilada"
+    @Override
+    public String ataqueHojaAfilada() {
+        throw new UnsupportedOperationException("Bulbasaur ataca con 'Hoja Afilada'"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    //metodo para uasr el ataque "Drenaje"
+    @Override
+    public String ataqueDrenaje() {
+        throw new UnsupportedOperationException("Bulbasaur ataca con 'Drenaje'"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    //metodo para uasr el ataque "Latigocepa"
+    @Override
+    public String ataqueLatigocepa() {
+        throw new UnsupportedOperationException("Bulbasaur ataca con 'Latigocepa'"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}

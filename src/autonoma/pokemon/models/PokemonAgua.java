36efaec5344ebbe9f896665d9c84c0de5ddad607
Juan Jose Morales Aristizabal <juanj.moralesa@autonoma.edu.co>
2025-04-01 16:20:30 -0500

    Implementacion de la interface Pokemon Agua

---
+++ src/autonoma/pokemon/models/PokemonAgua.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package autonoma.pokemon.models;
+
+/**
+ *
+ * @author juanb
+ */
+public interface PokemonAgua {
+    //metodo abstracto para el ataque "Hidro Bomba"
+    //@return String
+    public abstract String ataqueHidroBomba();
+    
+    //metodo abstracto para el ataque "ataque Pistola Agua"
+    //@return String
+    public abstract String ataquePistolaAgua();
+    
+    //metodo abstracto para el ataque "Burbuja"
+    //@return String
+    public abstract String ataqueBurbuja();
+    
+    //metodo abstracto para el ataque "Hidro Pulso"
+    //return String
+    public abstract String ataqueHidroPulso();
+}

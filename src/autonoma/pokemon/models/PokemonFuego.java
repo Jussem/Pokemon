db2f61af6c5caee6fadf0b459dc8f550012e2f68
Juan Jose Morales Aristizabal <juanj.moralesa@autonoma.edu.co>
2025-04-01 16:59:04 -0500

    Implementacion de la interfaz Pokemon Fuego

---
+++ src/autonoma/pokemon/models/PokemonFuego.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package autonoma.pokemon.models;
+
+/**
+ *
+ * @author juanb
+ */
+public interface PokemonFuego {
+    //metodo abstracto para el ataque "Puño Fuego"
+    //@return String
+    public abstract String ataquePunioFuego();
+    
+    //metodo abstracto para el ataque "Bola Fuego"
+    //return String
+    public abstract String ataqueBolaFuego();
+    
+    //metodo abstracto para el ataque "Lanzallamas"
+    //@return String
+    public abstract String ataqueLanzallamas();
+    
+    //metodo abstracto para el ataque "Ascuas"
+    //@return String
+    public abstract String ataqueAscuas();
+}

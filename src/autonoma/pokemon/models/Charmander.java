6f0cbd318a66bdefbbc62b086fdfa981135a3802
Juan Jose Morales Aristizabal <juanj.moralesa@autonoma.edu.co>
2025-04-01 17:41:52 -0500

    Implementacion de la clase Charmander

---
+++ src/autonoma/pokemon/models/Charmander.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.pokemon.models;
+
+/**
+ *
+ * @author juanb
+ */
+public class Charmander extends Pokemon implements PokemonFuego{
+    //CONSTRUCTOR
+    public Charmander(int numPokedex, String nombre, float pesoPokemon, String temporada){
+        super(numPokedex, nombre, pesoPokemon, temporada);
+    }
+    //metodo para uasr el ataque "Placaje"
+    @Override
+    public String ataquePlacaje() {
+        throw new UnsupportedOperationException("Charmander ataca con 'Placaje'"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    //metodo para uasr el ataque "Aranazo"
+    @Override
+    public String ataqueAranazo() {
+        throw new UnsupportedOperationException("Charmander ataca con 'Aranazo'"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    //metodo para uasr el ataque "Mordisco"
+    @Override
+    public String ataqueMordisco() {
+        throw new UnsupportedOperationException("Charmander ataca con 'Mordisco'"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    //metodo para uasr el ataque "Puño Fuego"
+    @Override
+    public String ataquePunioFuego() {
+        throw new UnsupportedOperationException("Charmander ataca con 'Puno Fuego'"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    //metodo para uasr el ataque "Bola Fuego"
+    @Override
+    public String ataqueBolaFuego() {
+        throw new UnsupportedOperationException("Charmander ataca con 'Bola Fuego'"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    //metodo para uasr el ataque "Lanzallamas"
+    @Override
+    public String ataqueLanzallamas() {
+        throw new UnsupportedOperationException("Charmander ataca con 'lanzallamas'"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    //metodo para uasr el ataque "Ascuas"
+    @Override
+    public String ataqueAscuas() {
+        throw new UnsupportedOperationException("Charmander ataca con 'Ascuas'"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
